2fdf8625e67a61190d039c5ebbbf1913958968bf
Ciaran Callaghan <ciarancallaghan94@gmail.com>
2020-07-26 20:12:34 +0100

    New Port

--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ app.set("views", "views");
 app.set("view engine", "pug");
 
 app.use("/", router);
-app.listen(process.env.port || 3000);
-console.log("Running at Port 3000");
+app.listen(process.env.port || 8080);
+console.log("Running at Port 8080");
