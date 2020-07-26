65f006e8599b132439cc8c2817c5e0dedec8d721
Ciaran Callaghan <ciarancallaghan94@gmail.com>
2020-07-26 16:34:29 +0100

    About to start Lambda migration

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 // dev
-module.exports = {
-  database: {
-    endpoint: "http://localhost:8000",
-    table: "MeSocialUsers",
-  },
-  region: "eu-west-1",
-};
-
-// prod
 // module.exports = {
 //   database: {
-//     endpoint: "https://dynamodb.eu-west-1.amazonaws.com",
+//     endpoint: "http://localhost:8000",
 //     table: "MeSocialUsers",
 //   },
 //   region: "eu-west-1",
 // };
+
+// prod
+module.exports = {
+  database: {
+    endpoint: "https://dynamodb.eu-west-1.amazonaws.com",
+    table: "MeSocialUsers",
+  },
+  region: "eu-west-1",
+};
