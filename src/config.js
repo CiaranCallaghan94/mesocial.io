a1f34c20d726916535e01ce44078089545917fe6
Ciaran Callaghan <ciarancallaghan94@gmail.com>
2020-08-22 23:32:01 +0100

    Updating Config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   database: {
     endpoint: "https://dynamodb.eu-west-1.amazonaws.com",
     // table: "MeSocialUsers",
-    table: "mesocial-users-dev",
+    table: "mesocial-users-prod",
   },
   region: "eu-west-1",
 };
